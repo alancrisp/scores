9d0c4ea4746bf27dec2ad0c9099c99d4da5a7a38
Alan Crisp <alan.crisp@gmail.com>
2020-09-27 13:54:03 +0100

    Remove sqlite config

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,7 @@ from flask import Flask
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'app.sqlite'),
+        SECRET_KEY='dev'
     )
 
     if test_config is None:
