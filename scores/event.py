86690b4a1e88ac07389534293db146a85543ab2e
Alan Crisp <alan.crisp@gmail.com>
2021-02-21 16:21:49 +0000

    Add event view page

--- scores/event.py
+++ scores/event.py
@@ -11,6 +11,16 @@ bp = Blueprint('event', __name__, url_prefix='/event')
 def events():
     return render_template('events.html')
 
+@bp.route('/<event_id>')
+def view(event_id=0):
+    cursor = db.connection.cursor()
+    cursor.execute(
+        '''SELECT e.*, c.name, c.holes FROM event e INNER JOIN course c USING (courseId) WHERE eventId = %s''',
+        (event_id)
+    )
+    event = cursor.fetchone()
+    return render_template('event-view.html', event=event)
+
 @bp.route('/create', methods=('GET', 'POST'))
 def create():
     cursor = db.connection.cursor()
